b98d364a52ae957359ffe4ef3a15e2f7af33cca5
Mohammed Khartoch <mohamed.khartoch@gmail.com>
2024-06-21 18:21:54 +0100

    nouveau logo

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ $title }}</title>
-        <link rel="shortcut icon" href="{{ asset('img/ENCGO.png') }}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('img/encgo_logo.png') }}" type="image/x-icon">
         <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
